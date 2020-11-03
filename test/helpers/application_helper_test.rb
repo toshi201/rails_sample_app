26da80b3c6a894201fa99c670539948b3517b69e
toshi201 <aquaconner8@gmail.com>
2020-11-03 12:20:13 +0900

    Implement advanced login

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -6,3 +6,21 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
   end
 end
+
+class SessionsHelperTest < ActionView::TestCase
+
+  def setup
+    @user = users(:michael)
+    remember(@user)
+  end
+
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
+
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+end
